d388a3d2d127f3a00547bc06c6fc7add56e139b1
khalidpqt <muhk4225@gmail.com>
2021-12-11 01:24:59 +0700

    yang terbaru dari kholid

--- MenuReset.cs
+++ MenuReset.cs
@@ -5,106 +5,139 @@ class MenuReset : BaseProgram
     public MenuReset()
     {
         bool menu = false;
-        do
-        {
-            //inisialisasi array
-            Dictionary<string, string> bencana = new Dictionary<string, string>();
-            bencana.Add("aa", "ab");
-            bencana.Add("ac", "ad");
-            bencana.Add("ae", "af");
-
-
-            bool doreapet = false;
-
-
-            do
-
-            {
-
-
-                Console.WriteLine("apakah anda ingin mereset ?(Y/N)");
-
-                string jawaban = Console.ReadLine();
-                jawaban = jawaban.ToUpper();
-
-                switch (jawaban)
-                {
-                    case "Y":
-                        doreapet = false;
-                        break;
-                    case "N":
-                        return;
-
-                    default:
-                        doreapet = true;
-                        Console.WriteLine("ulangi");
-
-                        continue;
-
-
-                }
-            }
-            while (doreapet);
-            bool selamat = false;
             do
             {
+                //inisialisasi array
+                Dictionary<string, string> bencana = new Dictionary<string, string>()
 
-                Console.WriteLine("Konfirmasi");
-                Console.WriteLine("Apakah anda yakin, ingin semua data ini direset ?(Y/N)");
-                string jawaban1 = Console.ReadLine();
-                jawaban1 = jawaban1.ToUpper();
-                switch (jawaban1)
                 {
-                    case "Y":
-                        selamat = false;
-                        break;
-                    case "N":
-                        return;
+                    {"aa", "ab"},
+                    {"ac", "ad"},
+                    {"30", "30"},
+                    {"40", "40"},
+                    {"50", "05"},
+                    {"60", "16"},
+                    {"70", "70"},
+                    {"80", "80"},
+                    {"90", "90"},
 
+                };
+                Console.WriteLine("Data Yang Tersimpan");
+                foreach (KeyValuePair<string, string> data in bencana)
+                {
 
+                    Console.WriteLine($"{data.Key} = {data.Value}");
 
-                    default:
-
-                        Console.WriteLine("Ulangi");
-                        selamat = true;
-                        continue;
                 }
+
+
+
+
+                bool doreapet = false;
+
+
+                do
+
+                {
+
+
+                    Console.WriteLine("apakah anda ingin mereset ?(Y/N)");
+
+                    string jawaban = Console.ReadLine();
+                    jawaban = jawaban.ToUpper();
+
+                    switch (jawaban)
+                    {
+                        case "Y":
+                            doreapet = false;
+
+                            break;
+                        case "N":
+                            return;
+                        case "":
+                            continue;
+
+                        default:
+                            doreapet = true;
+                            Console.WriteLine("ulangi");
+
+                            continue;
+
+
+                    }
+                }
+                while (doreapet);
+                bool selamat = false;
+                do
+                {
+
+                    Console.WriteLine("Konfirmasi");
+                    Console.WriteLine("Apakah anda yakin, ingin semua data ini direset ?(Y/N)");
+                    string jawaban1 = Console.ReadLine();
+                    jawaban1 = jawaban1.ToUpper();
+                    switch (jawaban1)
+                    {
+                        case "Y":
+                            selamat = false;
+
+                            break;
+                        case "N":
+                            return;
+
+
+
+                        default:
+
+                            Console.WriteLine("Ulangi");
+                            selamat = true;
+                            continue;
+                    }
+                }
+
+
+                while (selamat);
+
+                bencana.Clear();
+                bencana.Add("Undifined", "Undifined");
+                Console.WriteLine(bencana["Undifined"]);
+
+
+
+
+
+
+                bool mini = false;
+                do
+                {
+                    Console.WriteLine("data sudah undifined");
+                    Console.WriteLine("Apakah anda ingin kembali ke menu utama ?(Y/N)");
+                    string mana = Console.ReadLine();
+                    mana = mana.ToUpper();
+                    switch (mana)
+                    {
+                        case "Y":
+
+                            return;
+                        case "N":
+                            menu = true;
+
+                            break;
+
+                        default:
+                            Console.WriteLine("Ulangi");
+                            mini = true;
+
+                            continue;
+
+
+
+                    }
+
+                }
+                while (mini);
+
             }
-
-
-            while (selamat);
-
-            foreach (KeyValuePair<string, string> data in bencana)
-            {
-
-                Console.WriteLine($"{data.Key} : {data.Value}");
-
-            }
-
-
-
-            Console.WriteLine("data sudah undifined");
-            Console.WriteLine("Apakah anda ingin kembali ke menu utama ?(Y/N)");
-            string mana = Console.ReadLine();
-            mana = mana.ToUpper();
-            switch (mana)
-            {
-                case "Y":
-                    menu = true;
-                    break;
-                case "N":
-                    return;
-
-                default:
-                    Console.WriteLine("Ulangi");
-                    menu = true;
-
-                    continue;
-            }
-
-
-        }
-        while (menu);
+            while (menu);
 
     }
 }
